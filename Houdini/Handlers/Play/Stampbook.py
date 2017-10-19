eb42b2986c0e675c2de438f962fe30f7d8a628e4
Ben <ben@solero.me>
2017-10-19 23:21:46 +0100

    Tidy book cover handler and enforce XT data length

--- Houdini/Handlers/Play/Stampbook.py
+++ Houdini/Handlers/Play/Stampbook.py
@@ -39,22 +39,23 @@ def handleGetRecentStamps(self, data):
 
 @Handlers.Handle(XT.UpdateBookCover)
 def handleUpdateBookCover(self, data):
+    if not 4 <= len(data.StampCover) <= 10:
+        return
     bookCover = data.StampCover[0:4]
     color, highlight, pattern, icon = bookCover
-    if not(1 <= int(color) <= 6 \
-        and 1 <= int(highlight) <= 18 \
-        and -1 <= int(pattern) <= 6 \
-        and 1 <= int(icon) <= 6):
+    if not(1 <= int(color) <= 6 and 1 <= int(highlight) <= 18 \
+        and -1 <= int(pattern) <= 6 and 1 <= int(icon) <= 6):
         return
     for stamp in data.StampCover[4:10]:
-        stampType, stampId, posX, posY, rotation, depth = stamp.split("|")
+        stampArray = stamp.split("|")
+        if len(stampArray) != 6:
+            return
+        stampType, stampId, posX, posY, rotation, depth = stampArray
         stamps = self.user.Stamps.split("|")
         if stampId not in stamps:
             return
-        if not (0 <= int(stampType) <= 2 \
-                and 0 <= int(posX) <= 600 \
-                and 0 <= int(posY) <= 600 \
-                and 0 <= int(rotation) <= 360 \
+        if not (0 <= int(stampType) <= 2 and 0 <= int(posX) <= 600 \
+                and 0 <= int(posY) <= 600 and 0 <= int(rotation) <= 360 \
                 and 0 <= int(depth) <= 100):
             return
         bookCover.append(stamp)
