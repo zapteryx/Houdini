14271a9262fe7d22809043da573745d90a094425
Ben <ben@solero.me>
2017-10-22 21:25:05 +0100

    Fix bug that prevents clothing items being removed

--- Houdini/Handlers/Play/Setting.py
+++ Houdini/Handlers/Play/Setting.py
@@ -8,48 +8,56 @@ def handleSendUpdatePlayerColour(self, data):
 
 @Handlers.Handle(XT.UpdateHead)
 def handleSendUpdatePlayerHead(self, data):
-    if data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 2:
+    if (data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 2) \
+            or data.ItemId == 0:
         self.user.Head = data.ItemId
         self.room.sendXt("uph", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateFace)
 def handleSendUpdatePlayerFace(self, data):
-    if data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 3:
+    if (data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 3) \
+            or data.ItemId == 0:
         self.user.Face = data.ItemId
         self.room.sendXt("upf", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateNeck)
 def handleSendUpdatePlayerNeck(self, data):
-    if data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 4:
+    if (data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 4) \
+            or data.ItemId == 0:
         self.user.Neck = data.ItemId
         self.room.sendXt("upn", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateBody)
 def handleSendUpdatePlayerBody(self, data):
-    if data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 5:
+    if (data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 5) \
+            or data.ItemId == 0:
         self.user.Body = data.ItemId
         self.room.sendXt("upb", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateHand)
 def handleSendUpdatePlayerHand(self, data):
-    if data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 6:
+    if (data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 6) \
+            or data.ItemId == 0:
         self.user.Hand = data.ItemId
         self.room.sendXt("upa", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateFeet)
 def handleSendUpdatePlayerFeet(self, data):
-    if data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 7:
+    if (data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 7) \
+            or data.ItemId == 0:
         self.user.Feet = data.ItemId
         self.room.sendXt("upe", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateFlag)
 def handleSendUpdatePlayerFlag(self, data):
-    if data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 8:
+    if (data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 8) \
+            or data.ItemId == 0:
         self.user.Feet = data.ItemId
         self.room.sendXt("upl", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdatePhoto)
 def handleSendUpdatePlayerPhoto(self, data):
-    if data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 9:
+    if (data.ItemId in self.inventory and self.server.items[data.ItemId]["type"] == 9) \
+            or data.ItemId == 0:
         self.user.Photo = data.ItemId
         self.room.sendXt("upp", self.user.ID, data.ItemId)
