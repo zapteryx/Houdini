2694439c30810875436c37c47452aa13983d9b26
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2017-11-08 15:05:34 -0800

    Example plugin
    Contains an example of how to do just about everything with the plugin system.

---
+++ Houdini/Plugins/Example/__init__.py
@@ -0,0 +1,52 @@
+import zope.interface, logging
+from Houdini.Plugins import Plugin
+from Houdini.Handlers import Handlers
+from Houdini.Events import Events
+
+class Example(object):
+    zope.interface.implements(Plugin)
+
+    author = "Houdini Team"
+    version = 0.1
+    description = "A plugin to verify plugin system functionality and demonstrate implementation"
+
+    def __init__(self, server):
+        self.logger = logging.getLogger("Houdini")
+
+        self.server = server
+
+        Handlers.Login += self.handleLogin
+
+        # An error is thrown when attempting to (un)register in-game handlers on the login server
+        # You *could* check the server's type using self.server instead if you prefer that!
+        try:
+            Handlers.JoinWorld += self.handleJoinWorld
+            Handlers.JoinWorld -= self.handleJoinWorld
+        except TypeError:
+            pass
+
+        Events.Connected += self.handleConnection
+        Events.Disconnected += self.handleDisconnection
+
+    def handleJoinWorld(self, player, data):
+        self.logger.info("[Example] Holy smokes!")
+
+    def handleLogin(self, player, data):
+        self.logger.warn("[Example] %s is trying to login" % data.Username)
+
+    @Events.Register("Connected")
+    def handleConnected(player):
+        print("[Example] Woohoo~!")
+
+    @Events.Register("Disconnected")
+    def handleDisconnected(player):
+        print("[Example] Poo..~")
+
+    def handleConnection(self, player):
+        self.logger.info("[Example] New player connected, woohoo!")
+
+    def handleDisconnection(self, player):
+        self.logger.info("[Example] Aw, that sucks :-(")
+
+    def ready(self):
+        self.logger.info("Example plugin is ready!")
