14452ff0cd4e8bde07f6f3166c1d324bffc15116
Ben <ben@solero.me>
2018-01-18 22:45:53 +0000

    Table handlers

---
+++ Houdini/Handlers/Games/Table.py
@@ -0,0 +1,97 @@
+from Houdini.Handlers import Handlers, XT
+
+class Table(object):
+
+    def __init__(self, id, gameInstance):
+        self.id = id
+        self.game = gameInstance
+        self.penguins = []
+
+    def add(self, penguin):
+        self.penguins.append(penguin)
+
+        seatId = len(self.penguins) - 1
+
+        penguin.sendXt("jt", self.id, seatId + 1)
+        penguin.room.sendXt("ut", self.id, len(self.penguins))
+        penguin.table = self
+
+        return seatId
+
+    def remove(self, penguin):
+        self.penguins.remove(penguin)
+
+        penguin.sendXt("lt")
+        penguin.room.sendXt("ut", self.id, len(self.penguins))
+        penguin.table = None
+
+    def reset(self, penguin):
+        for penguin in self.penguins:
+            penguin.table = None
+
+        self.game = type(self.game)()
+        self.penguins = []
+        penguin.room.sendXt("ut", self.id, 0)
+
+    def getSeatId(self, penguin):
+        return self.penguins.index(penguin)
+
+    def getString(self):
+        if len(self.penguins) == 0:
+            return str()
+        elif len(self.penguins) == 1:
+            playerOne, = self.penguins
+            return "%".join([playerOne.user.Username, str(), self.game.getString()])
+        playerOne, playerTwo = self.penguins[:2]
+        if len(self.penguins) == 2:
+            return "%".join([playerOne.user.Username, playerTwo.user.Username, self.game.getString()])
+        return "%".join([playerOne.user.Username, playerTwo.user.Username, self.game.getString(), "1"])
+
+    def sendXt(self, *data):
+        for penguin in self.penguins:
+            penguin.sendXt(*data)
+
+def TableHandler(game):
+    def handlerFunction(function):
+        def handler(penguin, data):
+            if penguin.table and type(penguin.table.game) == game:
+                function(penguin, data)
+            return function
+        return handler
+    return handlerFunction
+
+def leaveTable(self):
+    if self.table:
+        seatId = self.table.getSeatId(self)
+        isPlayer = seatId < 2
+        gameReady = len(self.table.penguins) > 1
+        if isPlayer and gameReady:
+            self.table.sendXt("cz", self.user.Username)
+            self.table.reset(self)
+        else:
+            self.table.remove(self)
+
+@Handlers.Handle(XT.GetTablePopulation)
+def handleGetTablePopulation(self, data):
+    try:
+        tablesString = []
+        for tableId in data.Tables:
+            table = self.server.tables[int(tableId)]
+            tablesString.append("{}|{}".format(tableId, len(table.penguins)))
+        self.sendXt("gt", "%".join(tablesString))
+    except KeyError:
+        self.logger.warn("{} table does not exist.".format(self.user.Username))
+
+@Handlers.Handle(XT.JoinTable)
+def handleJoinTable(self, data):
+    try:
+        if not self.table:
+            table = self.server.tables[data.TableId]
+            table.add(self)
+    except KeyError:
+        self.logger.warn("{} tried to join a table which doesn't exist."
+                         .format(self.user.Username))
+
+@Handlers.Handle(XT.LeaveTable)
+def handleLeaveTable(self, data):
+    leaveTable(self)
