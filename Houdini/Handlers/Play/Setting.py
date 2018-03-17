83730a8c22916c9c40769ad6eb2fcb4986ba69c7
Ben <ben@solero.me>
2018-03-17 00:19:50 +0000

    Remove redundant throttle parameters

--- Houdini/Handlers/Play/Setting.py
+++ Houdini/Handlers/Play/Setting.py
@@ -1,14 +1,14 @@
 from Houdini.Handlers import Handlers, XT
 
 @Handlers.Handle(XT.UpdateColor)
-@Handlers.Throttle(1)
+@Handlers.Throttle()
 def handleSendUpdatePlayerColour(self, data):
     if data.ItemId in self.inventory and self.server.items.isItemColor(data.ItemId):
         self.user.Color = data.ItemId
         self.room.sendXt("upc", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateHead)
-@Handlers.Throttle(1)
+@Handlers.Throttle()
 def handleSendUpdatePlayerHead(self, data):
     if (data.ItemId in self.inventory and self.server.items.isItemHead(data.ItemId)) \
             or data.ItemId == 0:
@@ -16,7 +16,7 @@ def handleSendUpdatePlayerHead(self, data):
         self.room.sendXt("uph", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateFace)
-@Handlers.Throttle(1)
+@Handlers.Throttle()
 def handleSendUpdatePlayerFace(self, data):
     if (data.ItemId in self.inventory and self.server.items.isItemFace(data.ItemId)) \
             or data.ItemId == 0:
@@ -24,7 +24,7 @@ def handleSendUpdatePlayerFace(self, data):
         self.room.sendXt("upf", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateNeck)
-@Handlers.Throttle(1)
+@Handlers.Throttle()
 def handleSendUpdatePlayerNeck(self, data):
     if (data.ItemId in self.inventory and self.server.items.isItemNeck(data.ItemId)) \
             or data.ItemId == 0:
@@ -32,7 +32,7 @@ def handleSendUpdatePlayerNeck(self, data):
         self.room.sendXt("upn", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateBody)
-@Handlers.Throttle(1)
+@Handlers.Throttle()
 def handleSendUpdatePlayerBody(self, data):
     if (data.ItemId in self.inventory and self.server.items.isItemBody(data.ItemId)) \
             or data.ItemId == 0:
@@ -40,7 +40,7 @@ def handleSendUpdatePlayerBody(self, data):
         self.room.sendXt("upb", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateHand)
-@Handlers.Throttle(1)
+@Handlers.Throttle()
 def handleSendUpdatePlayerHand(self, data):
     if (data.ItemId in self.inventory and self.server.items.isItemHand(data.ItemId)) \
             or data.ItemId == 0 or self.server.items.isPuffle(data.ItemId):
@@ -48,7 +48,7 @@ def handleSendUpdatePlayerHand(self, data):
         self.room.sendXt("upa", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateFeet)
-@Handlers.Throttle(1)
+@Handlers.Throttle()
 def handleSendUpdatePlayerFeet(self, data):
     if (data.ItemId in self.inventory and self.server.items.isItemFeet(data.ItemId)) \
             or data.ItemId == 0:
@@ -56,7 +56,7 @@ def handleSendUpdatePlayerFeet(self, data):
         self.room.sendXt("upe", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateFlag)
-@Handlers.Throttle(1)
+@Handlers.Throttle()
 def handleSendUpdatePlayerFlag(self, data):
     if (data.ItemId in self.inventory and self.server.items.isItemPin(data.ItemId)) \
             or data.ItemId == 0:
@@ -64,7 +64,7 @@ def handleSendUpdatePlayerFlag(self, data):
         self.room.sendXt("upl", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdatePhoto)
-@Handlers.Throttle(1)
+@Handlers.Throttle()
 def handleSendUpdatePlayerPhoto(self, data):
     if (data.ItemId in self.inventory and self.server.items.isItemPhoto(data.ItemId)) \
             or data.ItemId == 0:
