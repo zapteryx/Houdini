da9ca8a03d4f53fa85a67431e50fd41511fa7d3e
Ben <ben@solero.me>
2018-03-21 23:41:57 +0000

    Update rank plugin for new database structure

--- Houdini/Plugins/Rank/__init__.py
+++ Houdini/Plugins/Rank/__init__.py
@@ -5,6 +5,7 @@ from sqlalchemy.exc import OperationalError
 from Houdini.Plugins import Plugin
 from Houdini.Handlers import Handlers
 from Houdini.Data.Penguin import Penguin
+from Houdini.Handlers.Play.Navigation import handleJoinWorld
 
 class Rank(object):
     zope.interface.implements(Plugin)
@@ -23,22 +24,23 @@ class Rank(object):
         Penguin.Rank = Column(Integer, nullable=False, server_default=text("'1'"))
 
         try:
-            self.server.databaseEngine.execute("ALTER TABLE penguins add Rank TINYINT(1) DEFAULT 1;")
+            self.server.databaseEngine.execute("ALTER TABLE penguin add Rank TINYINT(1) DEFAULT 1;")
 
         except OperationalError as opError:
             if "Duplicate column name" not in opError.message:
                 self.logger.warn(opError.message)
 
-        Handlers.Login += self.adjustMembershipDays
+        Handlers.JoinWorld += self.adjustMembershipDays
 
     def adjustMembershipDays(self, player, data):
         playerRank = player.session.query(Penguin.Rank). \
             filter(Penguin.ID == player.user.ID).scalar() - 1
 
-        if playerRank > 5:
+        if playerRank >= 5:
             playerRank = 4
 
-        player.membershipDays = self.membershipDaysByRank[playerRank]
+        player.age = Rank.membershipDaysByRank[playerRank]
+        self.logger.info(player.age)
 
     def ready(self):
         self.logger.info("Rank plugin is ready!")
