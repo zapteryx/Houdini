5c37af678c9e4ecf1eb82f3a623dbe041955b9c6
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2018-05-07 16:30:50 -0700

    IglooLike table model and detail columns
    Detail columns include location and locked status information

--- Houdini/Data/Igloo.py
+++ Houdini/Data/Igloo.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from sqlalchemy import Column, ForeignKey, Integer, SmallInteger, Table, text
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, SmallInteger, text
 from sqlalchemy.orm import relationship
 
 from Houdini.Data import Base
@@ -14,7 +14,8 @@ class Igloo(Base):
     Type = Column(SmallInteger, nullable=False, server_default=text("1"))
     Floor = Column(SmallInteger, nullable=False, server_default=text("0"))
     Music = Column(SmallInteger, nullable=False, server_default=text("0"))
-    Locked = Column(Integer, nullable=False, server_default=text("0"))
+    Location = Column(SmallInteger, nullable=False, server_default=text("0"))
+    Locked = Column(SmallInteger, nullable=False, server_default=text("0"))
 
     penguin = relationship(u'Penguin')
 
@@ -30,3 +31,19 @@ class IglooFurniture(Base):
     Rotation = Column(Integer, primary_key=True, nullable=False, server_default=text("0"))
 
     igloo = relationship(u'Igloo')
+
+
+class IglooLike(Base):
+    __tablename__ = 'igloo_likes'
+
+    IglooID = Column(ForeignKey(u'igloo.ID', ondelete=u'CASCADE', onupdate=u'CASCADE'), primary_key=True,
+                     nullable=False)
+    OwnerID = Column(ForeignKey(u'penguin.ID', ondelete=u'CASCADE', onupdate=u'CASCADE'), nullable=False, index=True)
+    PlayerID = Column(ForeignKey(u'penguin.ID', ondelete=u'CASCADE', onupdate=u'CASCADE'), primary_key=True,
+                      nullable=False, index=True)
+    Count = Column(Integer, nullable=False)
+    Date = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
+
+    igloo = relationship(u'Igloo')
+    penguin = relationship(u'Penguin', primaryjoin='IglooLike.OwnerID == Penguin.ID')
+    penguin1 = relationship(u'Penguin', primaryjoin='IglooLike.PlayerID == Penguin.ID')
