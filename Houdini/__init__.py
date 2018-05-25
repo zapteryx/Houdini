e3dfb60b8a5c84f6e7df1f1911bd78c669b44170
Spydar007 <root@spydar007.com>
2018-05-25 12:49:41 +0100

    Add date and time to log messages

--- Houdini/__init__.py
+++ Houdini/__init__.py
@@ -1,4 +1,4 @@
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG,format="%(asctime)s %(levelname)s:%(name)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
 logging.getLogger("beaker.container").setLevel(logging.INFO)
