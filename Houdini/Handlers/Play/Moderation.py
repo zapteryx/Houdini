6dcba46a55887d4e77a851fc8ffedf84e4ce06ed
Spydar007 <root@spydar007.com>
2018-05-25 10:49:52 +0100

    Update moderation handlers for AS3 and improve logic
    Remove redundant cheatKick, permaban players after 3 bans or if the ban is permanent, and correctly output this in the new style disconnect messages

--- Houdini/Handlers/Play/Moderation.py
+++ Houdini/Handlers/Play/Moderation.py
@@ -22,64 +22,60 @@ def handleKickPlayer(self, data):
     if self.user.Moderator:
         moderatorKick(self, data.PlayerId)
 
-def cheatKick(self, targetPlayer):
-    if targetPlayer in self.server.players:
-        self.server.players[targetPlayer].sendErrorAndDisconnect(800)
-
 def cheatBan(self, targetPlayer, banDuration=72, comment=""):
     if targetPlayer in self.server.players:
         target = self.server.players[targetPlayer]
-        numberOfBans = self.session.query(Ban).\
+        numberOfCheatingBans = self.session.query(Ban).\
             filter(Ban.PenguinID == targetPlayer).filter(Ban.Reason == 1).count()
-
-        dateIssued = datetime.now()
-        dateExpires = dateIssued + timedelta(hours=banDuration)
-        if numberOfBans < 1:
-            ban = Ban(PenguinID=targetPlayer, Issued=dateIssued, Expires=dateExpires,
-                      ModeratorID=None, Reason=1, Comment=comment)
-        else:
-            target.user.Permaban = True
-            ban = Ban(PenguinID=targetPlayer, Issued=dateIssued, Expires=dateExpires,
-                      ModeratorID=None, Reason=1, Comment=comment)
-
-        self.session.add(ban)
-
-        target.sendXt("e", 611, comment)
-        target.transport.loseConnection()
-
-def moderatorKick(self, targetPlayer):
-    if targetPlayer in self.server.players:
-        target = self.server.players[targetPlayer]
-        if not target.user.Moderator:
-            for (playerId, player) in self.server.players.items():
-                if player.user.Moderator:
-                    player.sendXt("ma", "k", targetPlayer, target.user.Username)
-            target.sendErrorAndDisconnect(5)
-
-def moderatorBan(self, targetPlayer, banDuration=24, comment=""):
-    target = self.session.query(Penguin).\
-        filter_by(ID=targetPlayer).first()
-
-    if not target.Moderator:
-        for (playerId, player) in self.server.players.items():
-            if player.user.Moderator:
-                player.sendXt("ma", "b", targetPlayer, target.Username)
 
         numberOfBans = self.session.query(Ban).\
             filter(Ban.PenguinID == targetPlayer).count()
 
         dateIssued = datetime.now()
         dateExpires = dateIssued + timedelta(hours=banDuration)
-        if numberOfBans < 3:
+
+        if numberOfCheatingBans >= 1 or numberOfBans >= 3:
+            target.user.Permaban = True
+            ban = Ban(PenguinID=targetPlayer, Issued=dateIssued, Expires=dateExpires,
+                      ModeratorID=None, Reason=1, Comment=comment)
+        else:
+            ban = Ban(PenguinID=targetPlayer, Issued=dateIssued, Expires=dateExpires,
+                      ModeratorID=None, Reason=1, Comment=comment)
+
+        self.session.add(ban)
+
+        target.sendXt("ban", "611", targetPlayer, "72")
+        target.transport.loseConnection()
+
+def moderatorKick(self, targetPlayer):
+    if targetPlayer in self.server.players:
+        target = self.server.players[targetPlayer]
+        if not target.user.Moderator:
+            target.sendXt("moderatormessage", "3", targetPlayer)
+            target.transport.loseConnection()
+
+def moderatorBan(self, targetPlayer, banDuration=24, comment=""):
+    target = self.session.query(Penguin).\
+        filter_by(ID=targetPlayer).first()
+
+    if not target.Moderator:
+        numberOfBans = self.session.query(Ban).\
+            filter(Ban.PenguinID == targetPlayer).count()
+
+        dateIssued = datetime.now()
+        dateExpires = dateIssued + timedelta(hours=banDuration)
+
+        if numberOfBans >= 3 or banDuration == 0:
+            target.Permaban = True
+            banDuration = 0
             ban = Ban(PenguinID=targetPlayer, Issued=dateIssued, Expires=dateExpires,
                       ModeratorID=self.user.ID, Reason=2, Comment=comment)
         else:
-            target.Permaban = True
             ban = Ban(PenguinID=targetPlayer, Issued=dateIssued, Expires=dateExpires,
                       ModeratorID=self.user.ID, Reason=2, Comment=comment)
 
         self.session.add(ban)
 
         if targetPlayer in self.server.players:
-            self.server.players[targetPlayer].sendXt("e", 610, comment)
+            self.server.players[targetPlayer].sendXt("ban", "612", None, banDuration, targetPlayer)
             self.server.players[targetPlayer].transport.loseConnection()
