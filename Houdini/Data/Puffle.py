8b46dcb3bc766c4bcea04734862d56a06d36e905
Arthur <somecatgirlrelatedthing@gmail.com>
2018-08-08 13:01:43 -0700

    New puffle table columns and care table definition
    Replace health and hunger with food and play respectively and added several new columns.

--- Houdini/Data/Puffle.py
+++ Houdini/Data/Puffle.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, text
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, text, SmallInteger
 from sqlalchemy.orm import relationship
 
 from Houdini.Data import Base
@@ -13,9 +13,22 @@ class Puffle(Base):
     Name = Column(String(16), nullable=False)
     AdoptionDate = Column(DateTime, nullable=False, server_default=text("current_timestamp()"))
     Type = Column(Integer, nullable=False)
-    Health = Column(Integer, nullable=False)
-    Hunger = Column(Integer, nullable=False)
+    Subtype = Column(Integer, nullable=False)
+    Food = Column(Integer, nullable=False)
+    Play = Column(Integer, nullable=False)
     Rest = Column(Integer, nullable=False)
+    Clean = Column(Integer, nullable=False)
     Walking = Column(Integer, server_default=text("0"))
+    Hat = Column(Integer, server_default=text("0"))
+    Backyard = Column(Integer, server_default=text("0"))
+
+    penguin = relationship(u'Penguin')
+
+class CareInventory(Base):
+    __tablename__ = 'care_inventory'
+
+    PenguinID = Column(ForeignKey(u'penguin.ID', ondelete=u'CASCADE', onupdate=u'CASCADE'), primary_key=True, nullable=False)
+    ItemID = Column(SmallInteger, primary_key=True, nullable=False, server_default=text("0"))
+    Quantity = Column(Integer, nullable=False, server_default=text("1"))
 
     penguin = relationship(u'Penguin')
