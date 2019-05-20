f91687a305ad88e2043057fd001620639bb5af0a
Ben <ben@solero.me>
2019-05-20 15:32:28 +0100

    Fix a bug where items could not be removed

--- Houdini/Handlers/Play/Setting.py
+++ Houdini/Handlers/Play/Setting.py
@@ -10,63 +10,56 @@ def handleSendUpdatePlayerColour(self, data):
 @Handlers.Handle(XT.UpdateHead)
 @Handlers.Throttle()
 def handleSendUpdatePlayerHead(self, data):
-    if (data.ItemId in self.inventory and self.server.items.isItemHead(data.ItemId)) \
-            or data.ItemId == 0:
+    if data.ItemId == 0 or(data.ItemId in self.inventory and self.server.items.isItemHead(data.ItemId)):
         self.user.Head = data.ItemId
         self.room.sendXt("uph", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateFace)
 @Handlers.Throttle()
 def handleSendUpdatePlayerFace(self, data):
-    if (data.ItemId in self.inventory and self.server.items.isItemFace(data.ItemId)) \
-            or data.ItemId == 0:
+    if data.ItemId == 0 or (data.ItemId in self.inventory and self.server.items.isItemFace(data.ItemId)):
         self.user.Face = data.ItemId
         self.room.sendXt("upf", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateNeck)
 @Handlers.Throttle()
 def handleSendUpdatePlayerNeck(self, data):
-    if (data.ItemId in self.inventory and self.server.items.isItemNeck(data.ItemId)) \
-            or data.ItemId == 0:
+    if data.ItemId == 0 or (data.ItemId in self.inventory and self.server.items.isItemNeck(data.ItemId)):
         self.user.Neck = data.ItemId
         self.room.sendXt("upn", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateBody)
 @Handlers.Throttle()
 def handleSendUpdatePlayerBody(self, data):
-    if (data.ItemId in self.inventory and self.server.items.isItemBody(data.ItemId)) \
-            or data.ItemId == 0:
+    if data.ItemId == 0 or (data.ItemId in self.inventory and self.server.items.isItemBody(data.ItemId)):
         self.user.Body = data.ItemId
         self.room.sendXt("upb", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateHand)
 @Handlers.Throttle()
 def handleSendUpdatePlayerHand(self, data):
-    if (data.ItemId in self.inventory and self.server.items.isItemHand(data.ItemId)) \
-            or data.ItemId == 0 or self.server.items.isPuffle(data.ItemId):
+    if data.ItemId == 0 or self.server.items.isPuffle(data.ItemId) or \
+            (data.ItemId in self.inventory and self.server.items.isItemHand(data.ItemId)):
         self.user.Hand = data.ItemId
         self.room.sendXt("upa", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateFeet)
 @Handlers.Throttle()
 def handleSendUpdatePlayerFeet(self, data):
-    if (data.ItemId in self.inventory and self.server.items.isItemFeet(data.ItemId)) \
-            or data.ItemId == 0:
+    if data.ItemId == 0 or (data.ItemId in self.inventory and self.server.items.isItemFeet(data.ItemId)):
         self.user.Feet = data.ItemId
         self.room.sendXt("upe", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdateFlag)
 @Handlers.Throttle()
 def handleSendUpdatePlayerFlag(self, data):
-    if (data.ItemId in self.inventory and self.server.items.isItemPin(data.ItemId)) \
-            or data.ItemId == 0:
+    if data.ItemId == 0 or (data.ItemId in self.inventory and self.server.items.isItemPin(data.ItemId)):
         self.user.Flag = data.ItemId
         self.room.sendXt("upl", self.user.ID, data.ItemId)
 
 @Handlers.Handle(XT.UpdatePhoto)
 @Handlers.Throttle()
 def handleSendUpdatePlayerPhoto(self, data):
-    if (data.ItemId in self.inventory and self.server.items.isItemPhoto(data.ItemId)) \
-            or data.ItemId == 0:
+    if data.ItemId == 0 or (data.ItemId in self.inventory and self.server.items.isItemPhoto(data.ItemId)):
         self.user.Photo = data.ItemId
         self.room.sendXt("upp", self.user.ID, data.ItemId)
